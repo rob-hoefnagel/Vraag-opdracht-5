ebc058668476fa0eeb0bb5d97f3c09fd1d5dc630
Rob.Hoefnagel <Rob.Hoefnagel@itris.nl>
2022-04-01 12:29:59 +0200

    Vraag opdracht 5

---
+++ Monteur.java
@@ -0,0 +1,26 @@
+/*Maak twee klassen aan: Monteur en Auto.
+De auto heeft booleans voor 4 kapotte onderdelen.
+In de constructor bepaal je met behulp van een array aan booleans welke onderdelen kapot zijn en niet.
+De toString methode toont je welke onderdelen kapot en niet kapot zijn. Het gaat om de onderdelen.
+- boolean motor;
+- boolean deur;
+- boolean voorruit;
+- boolean uitlaat;
+
+De monteur loopt deze booleans af en repareert ze.
+Het repareren heeft een prijs: elk onderdeel dat is gerepareerd kost 25 euro extra.*/
+
+class Monteur {
+    public static void main(String[] args) {
+        Auto auto1 = new Auto(true, false, true, false);
+
+    }
+
+}
+
+class  Auto {
+    public Auto(boolean motor, boolean deur, boolean voorruit, boolean uitlaat){
+
+
+    }
+}
